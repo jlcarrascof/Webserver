e9582b5208edc73c0dbfe530fc72cf84e3b5bb67
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-11 13:43:08 -0400

    Finish test now sending an HTML5 text

--- demo.js
+++ demo.js
@@ -1,8 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
 // console.log(http);
 
 http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hemos creado nuestro primer servidor web!\n 😄');
+    // * Enviando Texto Plano
+    // res.writeHead(200, {'Content-Type': 'text/plain'});
+    // res.end('Hemos creado nuestro primer servidor web!\n 😄');
+
+    // * Enviando HTML
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    const html = fs.readFileSync('./index.html');
+    res.end(html);
+    
 }).listen(3001, "localhost");
