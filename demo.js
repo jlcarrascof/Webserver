7cf6439b621f4ad85625ddac5f06b0840cc541fe
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-11 12:20:51 -0400

    Run npm init -y to install dependencies and some code in demo.js to create our first server

---
+++ demo.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+console.log(http);
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hemos creado nuestro primer servidor web!\n 😄');
+}).listen(3001, "localhost");
