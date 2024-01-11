30ca7e98db03e670c4174a9098f0a00664200ace
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-11 19:12:34 -0400

    Using console.log to check in nodemon the value of res = response

--- demo.js
+++ demo.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 // console.log(http);
 
 http.createServer((req, res) => {
+    console.log(res);
+
     // * Enviando Texto Plano
     // res.writeHead(200, {'Content-Type': 'text/plain'});
     // res.end('Hemos creado nuestro primer servidor web!\n 😄');
@@ -16,10 +18,33 @@ http.createServer((req, res) => {
 
     // * Enviando HTML con templates
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    let html = fs.readFileSync('./index.html', 'utf8');
-    const nombre = 'Luis';
-    html = html.replace('{nombre}', nombre);
-    res.end(html);
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    // let html = fs.readFileSync('./index.html', 'utf8');
+    // const nombre = 'Alberto';
+    // html = html.replace('{nombre}', nombre);
+    // res.end(html);
+
+    // * Enviando JSON
+
+    const alumnos = [
+        {
+            id: 1,
+            nombre: 'Nico',
+        },
+        {
+            id: 2,
+            nombre: 'Gus',
+        },
+        {
+            id: 3,
+            nombre: 'Irvin',
+        },
+        {
+            id: 4,
+            nombre: 'Joaquín',
+        }
+    ];
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(alumnos));
 
 }).listen(3001, "localhost");
