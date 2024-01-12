f49ed2bd2c8a17512bd436a68d563f2ca673e640
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-11 21:41:50 -0400

    Coding for url = / and /alumnos

--- demoRutas.js
+++ demoRutas.js
@@ -2,7 +2,35 @@ const fs = require('fs');
 const http = require('http');
 
 http.createServer((req, res) => {
-    console.log(req);
+    // console.log(req);
+    if (req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        let html = fs.readFileSync('./index.html', 'utf8');
+        const nombre = 'Alberto';
+        html = html.replace('{nombre}', nombre);
+        res.end(html);
+    } else if (req.url === '/alumnos') {
+        const alumnos = [
+            {
+                id: 1,
+                nombre: 'Nico',
+            },
+            {
+                id: 2,
+                nombre: 'Gus',
+            },
+            {
+                id: 3,
+                nombre: 'Irvin',
+            },
+            {
+                id: 4,
+                nombre: 'Joaquín',
+            }
+        ];
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(alumnos));            
+    }   
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Servidor Levantado');
+    res.end('Servidor Levantado 🚀');
 }).listen(3001, "localhost");
